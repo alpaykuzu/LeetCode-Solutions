f125d62ea9594c0688b9b1d57c72b2e69922315f
Alpay Kuzu <147747941+alpaykuzu@users.noreply.github.com>
2026-01-27 16:53:22 +0300

    Time: 0 ms (100%), Space: 46.7 MB (84.58%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int RemoveElement(int[] nums, int val) {
+         var count = 0;
+         foreach (var item in nums)
+         {
+             if (item != val)
+             {
+                 nums[count] = item;
+                 count++;
+             }
+         }
+         return count;
+    }
+}
